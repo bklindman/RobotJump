54dd7736aa9674d3f2323a48dbbd7ee0386d28be
bklindman <blake.lindman@gmail.com>
2016-11-14 18:21:59 -0500

    second commit

--- xjm/begin.c
+++ xjm/begin.c
@@ -29,73 +29,74 @@ void paint()
 {
 	float p;
 	int q,i1,j1;
-    beginPaint();
-
-    clearDevice();
+  beginPaint();
+  clearDevice();
 	setBrushColor(BLUE);
-	if (t<=10) {p=20*t-1*t*t; k=0;}
-	 else {p=100-1*(t-10)*(t-10);l=0;}
+	if (t<=10)
+	  {p=20*t-1*t*t; k=0;}
+	else
+	  {p=100-1*(t-10)*(t-10);l=0;}
 	rectangle(x-10,y-p-10,x+10,y-p+10);
-
-	setBrushColor(RED);
-	  for (i=1;i<=n+10;i=i+1)
+  setBrushColor(RED);
+  for (i=1;i<=n+10;i=i+1)
 	{
 	  for (j=1;j<=a[i];j++)
 	   roundrect(b[i][j]*10-30,(i-10)*10+p*l-5,b[i][j]*10+30,(i-10)*10+p*l+5,5,10);
 	}
 
-	
-	if (t==10 && l==1) {
-		               
-							for (i=70;i>=11;i--)
-							{a[i]=a[i-10];
-							for (j=1;j<=a[i];j++)
-							  b[i][j]=b[i-10][j];}
-						for (i=1;i<=10;i=i+2)
-						{ a[i]=1+(int)(rand()%1);
-		                    for (j=1;j<=a[i];j++)
-			                  b[i][j]=1+(int)(rand()%80);
-						}
-						l=0;
-	                   }
+	if (t==10 && l==1)
+  {
+	 for (i=70;i>=11;i--)
+	 {
+		a[i]=a[i-10];
+		for (j=1;j<=a[i];j++)
+			b[i][j]=b[i-10][j];
+	 }
+		for (i=1;i<=10;i=i+2)
+		{
+			a[i]=1+(int)(rand()%1);
+	    for (j=1;j<=a[i];j++)
+			  b[i][j]=1+(int)(rand()%80);
+		}
+			l=0;
+	}
 
-
-	 if (t-10>=0)  {
-            q=(y-p+14)/10+10;
-			for (j=1;j<=a[q];j++)
-				if ((x>=b[q][j]*10-30) && (x<=b[q][j]*10+30)) {t=0;
-					                                     y=y-p;
-														 l=1;
-					                                 
-														 k=1;
-				                                         break;}
-			q=(y-p+16)/10+10;
-			for (j=1;j<=a[q];j++)
-				if ((x>=b[q][j]*10-30) && (x<=b[q][j]*10+30)) {t=0;
-					                                     y=y-p;
-														 l=1;
-					                                 
-														 k=1;
-				                                         break;}
-			                     
-	                              }
-
-
-
-	 if ((t==28) && (k==0)) { paintText(400, 300, "The Game Over"); cancelTimer(1); }
-	
-
-    endPaint();
+	if (t-10>=0)
+	{
+    q=(y-p+14)/10+10;
+		for (j=1;j<=a[q];j++)
+		if ((x>=b[q][j]*10-30) && (x<=b[q][j]*10+30))
+		{
+			t=0;
+		  y=y-p;
+		  l=1;
+		  k=1;
+			break;
+		}
+		q=(y-p+16)/10+10;
+		for (j=1;j<=a[q];j++)
+		 if ((x>=b[q][j]*10-30) && (x<=b[q][j]*10+30))
+		 {
+			t=0;
+			y=y-p;
+		  l=1;
+		  k=1;
+		  break;
+		}
+	}
+  if ((t==28) && (k==0))
+	{
+		paintText(400, 300, "The Game Over"); cancelTimer(1);
+	}
+	endPaint();
 }
-
-
 
 void TimerEvent(int number)
 {
-    t=t+1;	
+  t=t+1;
 	t=t%30;
 	paint();
- 
+
 }
 
 void keyboardevent(int key, int event)
@@ -103,8 +104,12 @@ void keyboardevent(int key, int event)
 	if(event!=KEY_DOWN)
         return;
 	if(key==VK_SPACE)
-	{begingame();cancelTimer(2);}
-	else {  
+	{
+		begingame();
+		cancelTimer(2);
+	 }
+	else
+	{
 		switch(key)
     {
     case VK_LEFT:
@@ -113,20 +118,15 @@ void keyboardevent(int key, int event)
     case VK_RIGHT:
         x+=50;if (x>800) x=0;
         break;
+	  }
 	}
-	
-
-}
 }
 
 void begingame()
 {
-
-    x=400;y=600;
-	
-	
+  x=400;y=600;
 	srand((int)time(NULL));
-	for (i=1;i<=n+10;i=i+2) 
+	for (i=1;i<=n+10;i=i+2)
 	{
 		a[i]=1+(int)(rand()%1);
 		for (j=1;j<=a[i];j++)
@@ -134,30 +134,24 @@ void begingame()
 	}
 	a[70]=a[70]+1;
 	b[70][a[70]]=400;
-	
-	
-   startTimer(1,32);
+  startTimer(1,32);
 }
 
 int Setup()
 {
-	
-
 	initWindow("image",DEFAULT,DEFAULT,800,600);
 
 	registerTimerEvent(TimerEvent);
-	
+
 	loadImage("image.jpg",&img);
 
-	 beginPaint();
-    putImageScale(&img,0,0,800,600);
-    endPaint();
+	beginPaint();
+  putImageScale(&img,0,0,800,600);
+  endPaint();
 
 	loadSound("sound.mp3",&sound);
 	playSound(sound,1);
-	
+
 	registerKeyboardEvent(keyboardevent);
-    return 0;
-
-
+  return 0;
 }
